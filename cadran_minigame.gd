b232833bfbfe63fa34e023d2073a6dba07189720
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-14 12:56:11 +0200

    added victory and defeat

--- cadran_minigame.gd
+++ cadran_minigame.gd
@@ -6,6 +6,7 @@ var active: bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	aiguille = find_child("Aiguille")
+	aiguille.set_rotation_degrees(-135)
 	active = true
 
 func _rotateAiguille(delta):
@@ -18,3 +19,11 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("LeftClick"):
 		active = false
+		if aiguille.get_rotation_degrees() <= 55 && aiguille.get_rotation_degrees() >= 30:
+			print("win")
+		elif aiguille.get_rotation_degrees() < 30:
+			print("stalled")
+	
+	if aiguille.get_rotation_degrees() > 55:
+		active = false
+		print("bust")
