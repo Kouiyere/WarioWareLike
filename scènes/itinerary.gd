e237c25b8fab1c49b875e7ca795d4a3851399467
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-14 17:22:57 +0200

    basic logic done

--- scènes/itinerary.gd
+++ scènes/itinerary.gd
@@ -6,10 +6,16 @@ var starting_points: Array[Node]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	starting_points = find_children("Itinerary Start*")
+	for starting_point in starting_points:
+		starting_point.clicked.connect(wrongAnswer)
+	
 	answer = starting_points[randi() % starting_points.size()]
+	answer.clicked.disconnect(wrongAnswer)
+	answer.clicked.connect(rightAnswer)
 	print(answer)
 
+func rightAnswer():
+	print("Yeaaah")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func wrongAnswer():
+	print("You are lost")
