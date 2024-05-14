7bfa7020a38ae718fe1630b1aef558c9473c0361
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-14 16:10:47 +0200

    Clickable start

--- scènes/itinerary.gd
+++ scènes/itinerary.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-var answer: Sprite2D
-var starting_points: Node[]
+var answer: Area2D
+var starting_points: Array[Node]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	starting_points = find_children("Start?")
+	starting_points = find_children("Itinerary Start*")
+	answer = starting_points[randi() % starting_points.size()]
+	print(answer)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
