4c865418a41fad081ebe46e189a97f52e0ec5cea
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-15 17:00:38 +0200

    minigame fonctional

--- scènes/Itinerary End.gd
+++ scènes/Itinerary End.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
 @export var DESTINATION_VARIATIONS_ARRAY: Array = [
-	preload("res://graphismes/itinéraires 1.png"),
-	preload("res://graphismes/itinéraires 2.png"),
-	preload("res://graphismes/itinéraires 3.png")
+	preload("res://graphismes/destination 1.png"),
+	preload("res://graphismes/destination 2.png"),
+	preload("res://graphismes/destination 3.png")
 ]
+
+@export var end_points: Array[Node]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pickPath()
-
-func pickPath():
-	if DESTINATION_VARIATIONS_ARRAY.size() > 1:
+	end_points = find_children("End*")
+	for end_point in end_points:
 		var texture_id: int = randi() % DESTINATION_VARIATIONS_ARRAY.size()
-		var chosen_texture: Texture = DESTINATION_VARIATIONS_ARRAY[texture_id]
-		texture = chosen_texture
+		end_point.texture = DESTINATION_VARIATIONS_ARRAY[texture_id]
+		DESTINATION_VARIATIONS_ARRAY.remove_at(texture_id)
