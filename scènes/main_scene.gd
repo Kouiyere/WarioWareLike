47f17ff89bdf715307331f54e5675792283a9c74
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-16 16:11:17 +0200

    Linked win and lose signal

--- scènes/main_scene.gd
+++ scènes/main_scene.gd
@@ -15,5 +15,15 @@ func _ready():
 
 func loadMinigame():
 	var sceneID: int = randi() % MINIGAME_SCENES_ARRAY.size()
-	get_tree().change_scene_to_file(MINIGAME_SCENES_ARRAY[sceneID])
+	var path: String = MINIGAME_SCENES_ARRAY[sceneID]
+	var minigameScene = load(path)
+	var minigame = minigameScene.instantiate()
+	add_child(minigame)
+	minigame.win.connect(minigameWon)
+	minigame.lose.connect(minigameLost)
 
+func minigameWon():
+	print("you win")
+
+func minigameLost():
+	print("you lost")
