d37ebbd4b2f4d700178fcc2084dcd05f998abd87
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-16 17:57:15 +0200

    Added label and starting timer

--- scènes/fourneau_minigame.gd
+++ scènes/fourneau_minigame.gd
@@ -6,19 +6,26 @@ signal lose
 var jauge: Node2D
 @export var speed: float
 var clickArea: Area2D
+var label: RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	jauge = find_child("Jauge")
 	clickArea = find_child("ClickableArea")
-	clickArea.clicked.connect(moveJaugeUp)
+	label = find_child("RichTextLabel")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	jauge.position.y += speed * delta
-	
-	if jauge.position.y > 175 || jauge.position.y < -175:
-		lose.emit()
+	if $Timer.is_stopped():
+		clickArea.clicked.connect(moveJaugeUp)
+		jauge.position.y += speed * delta
+		
+		if jauge.position.y > 175 || jauge.position.y < -175:
+			lose.emit()
 
 func moveJaugeUp():
 	jauge.position.y -= 100
+
+
+func _on_timer_timeout():
+	pass # Replace with function body.
