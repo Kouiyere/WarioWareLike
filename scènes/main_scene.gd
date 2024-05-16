5c5bddf07853b3e2fef1fbf800196cb608127535
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-16 14:38:16 +0200

    main scene loads transition

--- scènes/main_scene.gd
+++ scènes/main_scene.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+var transitionScene: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	transitionScene = find_child("TransitionScene")
+	transitionScene.transitionEnd.connect(loadMinigame)
+	load("res://scènes/transition.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func loadMinigame():
+	print("launching Minigame")
