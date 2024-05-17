2a3cc87316884e6cb3ede15fe59b041e0ee9e9fc
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-17 14:44:12 +0200

    recovered backup for cadran minigame

--- scènes/cadran_minigame.gd
+++ scènes/cadran_minigame.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal win
+signal lose
+
 var aiguille: Node2D
 var active: bool
 
@@ -7,7 +10,7 @@ var active: bool
 func _ready():
 	aiguille = find_child("Aiguille")
 	aiguille.set_rotation_degrees(4.7)
-	active = true
+	active = false
 
 func _rotateAiguille(delta):
 	aiguille.set_rotation(aiguille.get_rotation() + delta * 5)
@@ -19,11 +22,17 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("LeftClick"):
 		active = false
-		if aiguille.get_rotation_degrees() <= 210 && aiguille.get_rotation_degrees() >= 190:
-			print("win")
-		elif aiguille.get_rotation_degrees() < 190:
-			print("stalled")
+		if aiguille.get_rotation_degrees() <= 214 && aiguille.get_rotation_degrees() >= 158:
+			win.emit()
+		elif aiguille.get_rotation_degrees() < 158:
+			lose.emit()
+		elif aiguille.get_rotation_degrees() > 214:
+			lose.emit()
 	
-	if aiguille.get_rotation_degrees() > 210:
+	if aiguille.get_rotation_degrees() > 265:
 		active = false
-		print("bust")
+		lose.emit()
+
+
+func _on_ready_timer_timeout():
+	active = true
