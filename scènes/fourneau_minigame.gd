69239f98d477eb16bef2e5eab56ff7a0592fce6f
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-05-17 12:16:47 +0200

    timer prefab

--- scènes/fourneau_minigame.gd
+++ scènes/fourneau_minigame.gd
@@ -7,16 +7,19 @@ var jauge: Node2D
 @export var speed: float
 var clickArea: Area2D
 var label: RichTextLabel
+var minigameTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	jauge = find_child("Jauge")
 	clickArea = find_child("ClickableArea")
 	label = find_child("RichTextLabel")
+	minigameTimer = find_child("Minigame Timer Clock")
+	minigameTimer.timeout.connect(timeoutReached)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $Timer.is_stopped():
+	if $"Ready Timer".is_stopped():
 		clickArea.clicked.connect(moveJaugeUp)
 		jauge.position.y += speed * delta
 		
@@ -26,6 +29,5 @@ func _process(delta):
 func moveJaugeUp():
 	jauge.position.y -= 100
 
-
-func _on_timer_timeout():
-	pass # Replace with function body.
+func timeoutReached():
+	win.emit()
